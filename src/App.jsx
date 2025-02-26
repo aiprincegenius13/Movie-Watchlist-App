516b4e19a4e5c13adff9eebd68b0f10b4cadaa97
aiprincegenius13 <ejblack369@icloud.com>
2025-02-26 13:54:48 -0700

    added some buttons and and hadings

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,57 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    
-    </>
+    <div>
+      <h1> Movie Reviews and Watchlist</h1>
+
+      <div className="container">
+        <div className="movie-container">
+          <h2>Movie List</h2>
+          <div className="movie-list">
+            <div className="movie">
+              <h3>Movie Title</h3>
+              <button>Add to Watchlist</button>
+              <button>Add to Watched</button>
+            </div>
+          </div>
+        </div>
+      </div>
+        <div className="watchlist-container">
+          <h2>Watchlist</h2>
+          <div className="watchlist">
+            <div className="movie">
+              <h3>Movie Title</h3>
+              <button>Remove from Watchlist</button>
+              <button>Add to Watched</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="watched-container">
+          <h2>Watched</h2>
+          <div className="watched">
+            <div className="movie">
+              <h3>Movie Title</h3>
+              <button>Remove from Watched</button>
+              <button>Add Review</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="review-container">
+          <h2>Reviews</h2>
+          <div className="reviews">
+            <div className="review">
+              <h3>Movie Title</h3>
+              <p>Review</p>
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
 
 export default App
+
+
+
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { MovieProvider } from './contexts/MovieContext';
+import { MovieProvider } from './Components/contexts/MovieContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
